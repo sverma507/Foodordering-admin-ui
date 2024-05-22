8c90c8d4ed54998f3acce05b40fe4f74c720c38d
CmBishnoi <bishnoicm0029@gmail.com>
2024-05-22 11:16:59 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import Navbar from './components/Navbar1';
+import Navbar1 from './components/Navbar1';
+import Home from './components/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar1 />
   </React.StrictMode>
 );
 
