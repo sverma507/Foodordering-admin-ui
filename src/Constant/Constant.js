4b66ca6c3f87cfffde92c1ce08d6144dfe02d9c8
sverma507 <148950306+sverma507@users.noreply.github.com>
2024-07-26 15:08:24 +0530

    Update Constant.js

--- src/Constant/Constant.js
+++ src/Constant/Constant.js
@@ -1,2 +1,2 @@
-// export const URL='https://backend-foodordering-aq49.onrender.com'
-export const URL='http://localhost:4000'
+export const URL='https://backend-foodordering-aq49.onrender.com'
+// export const URL='http://localhost:4000'
